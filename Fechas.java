53d26a96307d0603c6b402807d61a70f61aa244c
Pablodd <pablo.perez92@hotmail.com>
2022-11-04 11:22:15 +0100

    add: Style

--- Fechas.java
+++ Fechas.java
@@ -2,46 +2,91 @@ import java.util.Scanner;
 
 public class Fechas {
 
+    static public float hora_1;
+    static public float minute_1;
+    static public float hora_2;
+    static public float minute_2;
+    static public float calculate_minutes;
+    static public float incognita;
+
+    static String franjas(String franja_1, String franja_2){
+
+        if(franja_1.contains("am")){
+            String hour_1_am = franja_1.replace("am", "");
+            String[] hour_minute_1 = hour_1_am.split(":");
+
+            hora_1 = Float.parseFloat(hour_minute_1[0]);
+            minute_1 = Float.parseFloat(hour_minute_1[1]);
+
+        }else if(franja_1.contains("pm")){
+            String hour_1_am = franja_1.replace("pm", "");
+            String[] hour_minute_1 = hour_1_am.split(":");
+
+            hora_1 = Float.parseFloat(hour_minute_1[0]);
+            minute_1 = Float.parseFloat(hour_minute_1[1]);
+
+        }else{
+            System.out.println("Algo falló");
+        }
+
+        if(franja_2.contains("am")){
+            String hour_2_am = franja_2.replace("am", "");
+            String[] hour_minute_2 = hour_2_am.split(":");
+
+            hora_2 = Float.parseFloat(hour_minute_2[0]);
+            minute_2 = Float.parseFloat(hour_minute_2[1]);
+
+        }else if(franja_2.contains("pm")){
+            String hour_2_am = franja_2.replace("pm", "");
+            String[] hour_minute_2 = hour_2_am.split(":");
+
+            hora_2 = Float.parseFloat(hour_minute_2[0]);
+            minute_2 = Float.parseFloat(hour_minute_2[1]);
+
+        }else{
+            System.out.println("Algo falló");
+        }
+
+        if( hora_1 > 12){
+
+            return "No podemos seguir";
+
+        }else if( hora_2 > 12){
+
+            return "No podemos seguir";
+     
+        }else{
+            
+            return  Suma_horas(hora_1, minute_1, hora_2, minute_2);
+            
+        }
+
+    }
+
     static String Suma_horas(float h_1, float m_1, float h_2, float m_2){
         
+        if(h_2<h_1){
 
-        if(h_2>h_1){
-
-            float calculate_minutes = (((h_2*60)+(m_2))-((h_1*60)+(m_1)))/60;
-    
-            float parte_decimal = calculate_minutes % 1;
-            float parte_entera = calculate_minutes - parte_decimal;
-
-            float parte_decimal_minutos = parte_decimal*60;
-
-
-            int minutos = (int) parte_decimal_minutos;
-            int horas = (int) parte_entera;
-
-            
-
-            String diferencia_horas = horas + ":" + minutos;
-
-            return diferencia_horas;
+            incognita = h_2 + 12;
 
         }else{
 
-            float calculate_minutes = ((((h_2+12)*60)+(m_2))-((h_1*60)+(m_1)))/60;
-
-            float parte_decimal = calculate_minutes % 1;
-            float parte_entera = calculate_minutes - parte_decimal;
-
-            float parte_decimal_minutos = parte_decimal*60;
-
-
-            int minutos = (int) parte_decimal_minutos;
-            int horas = (int) parte_entera;
-
-            String diferencia_horas = horas + ":" + minutos;
-
-            return diferencia_horas;
+            incognita = h_2;
 
         }
+        float calculate_minutes = (((incognita*60)+(m_2))-((h_1*60)+(m_1)))/60;
+    
+        float parte_decimal = calculate_minutes % 1;
+        float parte_entera = calculate_minutes - parte_decimal;
+
+        float parte_decimal_minutos = parte_decimal*60;
+
+        int minutos = (int) parte_decimal_minutos;
+        int horas = (int) parte_entera;
+  
+        String diferencia_horas = horas + ":" + minutos;
+
+        return diferencia_horas;
 
     }
 
@@ -56,146 +101,8 @@ public class Fechas {
 
         String franja_1 = parts[0].replace(" ","");
         String franja_2 = parts[1].replace(" ","");
-
-        if(franja_1.contains("am")){
-
-           if(franja_2.contains("am")){
-            String hour_1_am = franja_1.replace("am", "");
-            String hour_2_am = franja_2.replace("am", "");
-
-            String[] hour_minute_1 = hour_1_am.split(":");
-            String[] hour_minute_2 = hour_2_am.split(":");
-
-
-            float hora_1 = Float.parseFloat(hour_minute_1[0]);
-            float minute_1 = Float.parseFloat(hour_minute_1[1]);
-
-            float hora_2 = Float.parseFloat(hour_minute_2[0]);
-            float minute_2 = Float.parseFloat(hour_minute_2[1]);
-
-            if(hora_1>12){
-
-                System.out.println("No podemos seguir");
-
-
-            }else if(hora_2>12){
-
-                System.out.println("No podemos seguir");
-
-                
-            }else{
-
-                System.out.print("Hay una franja de: " +Suma_horas(hora_1, minute_1, hora_2, minute_2));
-                
-            }
-
-           }else if(franja_2.contains("pm")){
-            String hour_1_am = franja_1.replace("am", "");
-            String hour_2_am = franja_2.replace("pm", "");
-
-            String[] hour_minute_1 = hour_1_am.split(":");
-            String[] hour_minute_2 = hour_2_am.split(":");
-
-
-            float hora_1 = Float.parseFloat(hour_minute_1[0]);
-            float minute_1 = Float.parseFloat(hour_minute_1[1]);
-
-            float hora_2 = Float.parseFloat(hour_minute_2[0]);
-            float minute_2 = Float.parseFloat(hour_minute_2[1]);
-
-            if(hora_1>12){
-
-                System.out.println("No podemos seguir");
-
-
-            }else if(hora_2>12){
-
-                System.out.println("No podemos seguir");
-
-                
-            }else{
-
-                System.out.print("Hay una franja de: " +Suma_horas(hora_1, minute_1, hora_2, minute_2));
-                
-            }
-            
-
-           }else{
-            System.out.println("No podemos calcular la hora");
-
-           }
- 
-        }else if(franja_1.contains("pm")){
-
-            if(franja_2.contains("am")){
-                String hour_1_am = franja_1.replace("pm", "");
-                String hour_2_am = franja_2.replace("am", "");
-    
-                String[] hour_minute_1 = hour_1_am.split(":");
-                String[] hour_minute_2 = hour_2_am.split(":");
-    
-    
-                float hora_1 = Float.parseFloat(hour_minute_1[0]);
-                float minute_1 = Float.parseFloat(hour_minute_1[1]);
-    
-                float hora_2 = Float.parseFloat(hour_minute_2[0]);
-                float minute_2 = Float.parseFloat(hour_minute_2[1]);
-    
-                if(hora_1>12){
-    
-                    System.out.println("No podemos seguir");
-    
-    
-                }else if(hora_2>12){
-    
-                    System.out.println("No podemos seguir");
-    
-                    
-                }else{
-    
-                    System.out.print("Hay una franja de: " +Suma_horas(hora_1, minute_1, hora_2, minute_2));
-                    
-                }
-    
-               }else if(franja_2.contains("pm")){
-                String hour_1_am = franja_1.replace("pm", "");
-                String hour_2_am = franja_2.replace("pm", "");
-    
-                String[] hour_minute_1 = hour_1_am.split(":");
-                String[] hour_minute_2 = hour_2_am.split(":");
-    
-    
-                float hora_1 = Float.parseFloat(hour_minute_1[0]);
-                float minute_1 = Float.parseFloat(hour_minute_1[1]);
-    
-                float hora_2 = Float.parseFloat(hour_minute_2[0]);
-                float minute_2 = Float.parseFloat(hour_minute_2[1]);
-    
-                if(hora_1>12){
-    
-                    System.out.println("No podemos seguir");
-    
-    
-                }else if(hora_2>12){
-    
-                    System.out.println("No podemos seguir");
-    
-                    
-                }else{
-    
-                    System.out.print("Hay una franja de: " +Suma_horas(hora_1, minute_1, hora_2, minute_2));
-                    
-                }
-                
-    
-               }else{
-                System.out.println("No podemos calcular la hora");
-    
-               }
-
-        }else{
-            System.out.println("Algo salio mal");
-        }
+     
+        System.out.println(franjas(franja_1, franja_2));
 
     }
     
